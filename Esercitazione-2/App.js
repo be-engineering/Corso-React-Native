5b8666677258820e5bdc55f70a83fd661061877f
Christian Chiama <christian.chiama@atcult.it>
2019-03-12 13:12:15 +0100

    add files to second project
    add files to second project

---
+++ Esercitazione-2/App.js
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import { View, Text, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
+import { ListItem, SearchBar } from "react-native-elements";
+
+class FlatListDemo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false
+    };
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+    this.setState({ loading: true });
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
+  handleRefresh = () => {
+    this.setState(
+      {
+        page: 1,
+        seed: this.state.seed + 1,
+        refreshing: true
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+  };
+
+  handleLoadMore = () => {
+    this.setState(
+      {
+        page: this.state.page + 1
+      },
+      () => {
+        this.makeRemoteRequest();
+      }
+    );
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />
+    );
+  };
+
+  renderHeader = () => {
+    return <SearchBar placeholder="Type Here..." lightTheme round />;
+  };
+
+  renderFooter = () => {
+    if (!this.state.loading) return null;
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderTopWidth: 1,
+          borderColor: "#CED0CE"
+        }}
+      >
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <View style={style.container}>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              leftAvatar={{ source: { uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' } }}
+              title={`${item.name.first} ${item.name.last}`}
+              subtitle={item.email}
+              avatar={{ uri: item.picture.thumbnail }}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />
+          )}
+          keyExtractor={item => item.email}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+          ListFooterComponent={this.renderFooter}
+          onRefresh={this.handleRefresh}
+          refreshing={this.state.refreshing}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={50}
+        />
+        </View>
+    );
+  }
+}
+
+export default FlatListDemo;
+
+
+const style = StyleSheet.create({
+  container: {
+    marginTop: 36
+  }
+})
---
+++ Esercitazione-2/src/LottieWrapper.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Animation from 'lottie-react-native';
+import { View, StyleSheet, Animated } from 'react-native';
+
+type Props = {
+   anim: {},
+   width: number,
+   height: number,
+   duration: number,
+   loop: bool;
+}
+
+type State = {
+ progress: number;
+}
+
+export default class LottieWrapper extends React.Component<Props, State> {
+ constructor(props:Props) {
+   super(props);
+   this.state = {
+     progress: new Animated.Value(0),
+   };
+ }
+
+ componentDidMount() {
+   this.animation.play();
+ }
+
+ animatedProgressive() {
+   const { progress } = this.state;
+   const { duration } = this.props;
+   Animated.timing(progress, {
+     toValue: 1,
+     duration,
+   }).start();
+ }
+
+ render() {
+   const {
+     width, height, anim, loop,
+   } = this.props;
+   return (
+     <View style={styles.container}>
+       <View>
+         <Animation
+           ref={(animation) => {
+             this.animation = animation;
+           }}
+           style={{
+             width: width || 280,
+             height: height || 280,
+           }}
+           loop={loop || true}
+           source={anim}
+         />
+       </View>
+     </View>
+   );
+ }
+}
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   backgroundColor: '#ffffff'
+ },
+});
